804a2fb2f68abfe5efea1288401a7ee1cf4e4b3c
steele19890325 <530631941@qq.com>
2015-08-12 20:38:51 +0800

    change once

--- Test20150812/Test20150812/ViewController.m
+++ Test20150812/Test20150812/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 40, 40, 30)];
 }
 
 - (void)didReceiveMemoryWarning {
